153968d5d7a56f265354f36fe8c7b21da31b8fc1
patrickduhaime <32279890+patrickduhaime@users.noreply.github.com>
2019-05-02 22:27:39 -0400

    screenGrab.py

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # pif6004
+
+ScreenGrab.py  ==>  reduire 'affichage de l'écran a 80% (CTRL et - ) pour voir tout l'ecran sur la page.
---
+++ screenGrab.py
@@ -0,0 +1,15 @@
+from PIL import ImageGrab
+import os
+import time
+ 
+def screenGrab():
+    box = (189,277,887,668)
+    im = ImageGrab.grab(box)
+    im.save(os.getcwd() + '\\full_snap__' + str(int(time.time())) +
+'.png', 'PNG')
+ 
+def main():
+    screenGrab()
+ 
+if __name__ == '__main__':
+    main()
