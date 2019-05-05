c88ea8b6eca5941fcac62e4cc46bcb28dcba8914
patrickduhaime <32279890+patrickduhaime@users.noreply.github.com>
2019-05-05 15:32:56 -0400

    Ballade a St-Valere

--- screenGrab.py
+++ screenGrab.py
@@ -291,12 +291,12 @@ def screenGrab():
         new_screen,original_image, m1, m2 = process_img(screen)
         cv2.imshow('window',cv2.cvtColor(original_image, cv2.COLOR_BGR2RGB))
 		
-        if m1 < 0 and m2 < 0:
-            right()
-        elif m1 > 0  and m2 > 0:
-            left()
-        else:
-            straight()
+        #if m1 < 0 and m2 < 0:
+            #right()
+        #elif m1 > 0  and m2 > 0:
+            #left()
+        #else:
+            #straight()
 		
         if cv2.waitKey(25) & 0xFF == ord('q'):
             cv2.destroyAllWindows()
