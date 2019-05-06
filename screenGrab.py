fa1aeaabfe8b6e3c0012f3e631ecac67d7099eb5
patrickduhaime <32279890+patrickduhaime@users.noreply.github.com>
2019-05-06 13:33:13 -0400

    data training begins

--- screenGrab.py
+++ screenGrab.py
@@ -291,12 +291,14 @@ def screenGrab():
         new_screen,original_image, m1, m2 = process_img(screen)
         cv2.imshow('window',cv2.cvtColor(original_image, cv2.COLOR_BGR2RGB))
 		
-        #if m1 < 0 and m2 < 0:
-            #right()
-        #elif m1 > 0  and m2 > 0:
-            #left()
-        #else:
-            #straight()
+        if m1 < 0 and m2 < 0:
+            right()
+        elif m1 > 0  and m2 > 0:
+            left()
+        elif m1 > 0 and m2 < 0:
+            straight()
+        else:
+            brake()
 		
         if cv2.waitKey(25) & 0xFF == ord('q'):
             cv2.destroyAllWindows()
