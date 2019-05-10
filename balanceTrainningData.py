dc4123ce1274337c2024588cffc880498f5905a2
patrickduhaime <32279890+patrickduhaime@users.noreply.github.com>
2019-05-10 14:01:43 -0400

    added showTrainningData and balanceTrainningData

---
+++ balanceTrainningData.py
@@ -0,0 +1,47 @@
+# balance_data.py
+
+import numpy as np
+import pandas as pd
+from collections import Counter
+from random import shuffle
+
+train_data = np.load('training_data.npy')
+
+lefts = []
+rights = []
+forwards = []
+brakes = []
+do_nothings = []
+
+shuffle(train_data)
+
+for data in train_data:
+    img = data[0]
+    choice = data[1]
+
+    if choice == [1,0,0,0]:
+        lefts.append([img,choice])
+    elif choice == [0,1,0,0]:
+        forwards.append([img,choice])
+    elif choice == [0,0,1,0]:
+        rights.append([img,choice])
+    elif choice == [0,0,0,1]:
+        brakes.append([img,choice])
+    elif choice == [0,0,0,0]:
+        do_nothings.append([img,choice])
+    else:
+        print('no matches')
+
+
+do_nothings = do_nothings[:len(lefts)][:len(rights)]
+lefts = lefts[:len(do_nothings)]
+rights = rights[:len(do_nothings)]
+
+forwards = forwards[:len(lefts)][:len(rights)]
+lefts = lefts[:len(forwards)]
+rights = rights[:len(forwards)]
+
+final_data = do_nothings + forwards + lefts + rights + brakes
+shuffle(final_data)
+
+np.save('training_data_v2.npy', final_data)
