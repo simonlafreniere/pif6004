ca60c675f9689b9b10c99a4c8ee024f8334f9f68
patrickduhaime <32279890+patrickduhaime@users.noreply.github.com>
2019-05-10 15:13:12 -0400

    added alexnet model file

--- showTrainningData.py
+++ showTrainningData.py
@@ -5,8 +5,14 @@ import pandas as pd
 from collections import Counter
 from random import shuffle
 
-train_data = np.load('training_data_v2.npy')
-
+balanced_train_data = np.load('training_data_v2.npy')
+train_data = np.load('training_data.npy')
 df = pd.DataFrame(train_data)
-print(df.head())
+bdf = pd.DataFrame(balanced_train_data)
+print('-----------------data-------------------------')
 print(Counter(df[1].apply(str)))
+print('----------------------------------------------')
+print('')
+print('-------------balanced data--------------------')
+print(Counter(bdf[1].apply(str)))
+print('----------------------------------------------')
