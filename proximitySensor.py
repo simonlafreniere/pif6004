18e855668490cbfb749166410b882d4d3b1ac55a
patrickduhaime <32279890+patrickduhaime@users.noreply.github.com>
2019-05-18 20:30:21 -0400

    update proximitySensor to return center position

--- proximitySensor.py
+++ proximitySensor.py
@@ -1,8 +1,11 @@
 from imageai.Detection import ObjectDetection
 import os
+import re
 
 execution_path = os.getcwd()
+boxPosition = []
 result = []
+
 def crop(image):
     return image[170:,:]
 
@@ -14,7 +17,13 @@ def getTraffic(img):
     detector.loadModel(detection_speed="fastest")
     detections = detector.detectObjectsFromImage(input_image=image, input_type="array", minimum_percentage_probability=30)
     for eachObject in detections:
-        result.append(eachObject["box_points"])
+        boxPosition.append(eachObject["box_points"])
+    for eachObject in boxPosition:
+        eachObject = re.sub('[()]', '', str(eachObject))
+        x1,y1,x2,y2 = eachObject.split(',')
+        x =(int(x1)+int(x2))/2
+        y =(int(y1)+int(y2))/2
+        result.append("(" + str(int(x)) + ", " + str(int(y)) + ")")
     return result
 
 def main():
