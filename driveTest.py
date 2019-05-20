6fd8f96fa8d04b30dea25a1b97e9f431aac57306
patrickduhaime <32279890+patrickduhaime@users.noreply.github.com>
2019-05-20 13:22:56 -0400

    update driveTest

--- driveTest.py
+++ driveTest.py
@@ -1,5 +1,4 @@
 from laneSensor import findLanes
-from positionSensor import getPosition
 #from proximitySensor import getTraffic
 from speedSensor import getSpeed
 from PIL import ImageGrab
@@ -22,11 +21,24 @@ Play area =  x_pad+1, y_pad+1, x_pad+805, y_pad+461
 x_pad = 271
 y_pad = 236
 box = (x_pad+1, y_pad+1, x_pad+805, y_pad+461)
+positionX = 400
+positionY = 155
 
 while(True):
     img = np.array(ImageGrab.grab(box))
     speed = getSpeed()
-    position = getPosition(img)
     lane1,lane2 = findLanes(img)
-	#traffic = getTraffic(img)
-    print(speed, position, lane1, lane2)
+    #traffic = getTraffic(img)
+
+    if m1 < 0 and m2 < 0:
+        right()
+    elif m1 > 0 and m2 > 0:
+        left()
+    else:
+        if(speed<15):
+            straight()
+        if(speed>20):
+            release()
+        if(speed>25):
+            brake()
+
