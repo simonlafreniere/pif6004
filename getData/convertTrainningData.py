353ed0ca841f308aaa5fbc1ddcb9742780e4cbd3
patrickduhaime <32279890+patrickduhaime@users.noreply.github.com>
2019-05-29 14:35:17 -0400

    .tif 2 .jpeg

--- getData/convertTrainningData.py
+++ getData/convertTrainningData.py
@@ -13,14 +13,14 @@ right = 0
 
 for i in train_data:
     if(i[1] == [1,0,0,0]):
-        scipy.misc.imsave(path + '/left/' + str(left)  + '.tif', i[0])
+        scipy.misc.imsave(path + '/left/' + str(left)  + '.jpeg', i[0])
         left +=1
     if(i[1] == [0,0,1,0]):
-        scipy.misc.imsave(path + '/right/' + str(right)  + '.tif', i[0])
+        scipy.misc.imsave(path + '/right/' + str(right)  + '.jpeg', i[0])
         right +=1
     if(i[1] == [0,0,0,1]):
-        scipy.misc.imsave(path + '/brake/' + str(brake)  + '.tif', i[0])
+        scipy.misc.imsave(path + '/brake/' + str(brake)  + '.jpeg', i[0])
         brake +=1
     if(i[1] == [0,1,0,0]):
-        scipy.misc.imsave(path + '/forward/' + str(forward)  + '.tif', i[0])
+        scipy.misc.imsave(path + '/forward/' + str(forward)  + '.jpeg', i[0])
         forward +=1
